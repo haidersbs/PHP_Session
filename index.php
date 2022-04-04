59f2bc47a8ce8bc65dfddb7de60959ce569a6cf7
Md Sumon Ali <69789708+mdsumonali372@users.noreply.github.com>
2022-04-04 22:49:01 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+
+$_SESSION['username'] = "md abu sayed";
+$_SESSION['password'] = 1234;
+
+
+
+?>
+
+
+
+<!-- je file e session create kora thakbe se file ta obbosoi run korte hbe tahole onno sokol page theke access kora jabe -->
+<h1>this is our index page</h1>
